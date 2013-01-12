fd7984829d26cb0c63ecbfc5fa63350e05a6d6b3
Eric GELOEN <geloen.eric@gmail.com>
2013-01-12 18:08:26 +0100

    [Form][Type] Allow to set all config options (#18)

--- Form/Type/CKEditorType.php
+++ Form/Type/CKEditorType.php
@@ -28,21 +28,17 @@ class CKEditorType extends AbstractType
      */
     public function buildForm(FormBuilder $builder, array $options)
     {
-        $builder
-            ->setAttribute('toolbar', $options['toolbar'])
-            ->setAttribute('ui_color', $options['ui_color']);
+        $builder->setAttribute('config', $options['config']);
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function buildView(FormView $view, FormInterface $form)
     {
-        $view
-            ->set('toolbar', $form->getAttribute('toolbar'))
-            ->set('ui_color', $form->getAttribute('ui_color'));
+        $view->set('config', $form->getAttribute('config'));
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -50,58 +46,10 @@ class CKEditorType extends AbstractType
     {
         return array(
             'required' => false,
-            'toolbar' => array(
-                array(
-                    'name' => 'document',
-                    'items' => array('Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates')
-                ),
-                array(
-                    'name' => 'clipboard',
-                    'items' => array('Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo')
-                ),
-                array(
-                    'name' => 'editing',
-                    'items' => array('Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt')
-                ),
-                array(
-                    'name' => 'forms',
-                    'items' => array('Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField')
-                ),
-                '/',
-                array(
-                    'name' => 'basicstyles',
-                    'items' => array('Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat')
-                ),
-                array(
-                    'name' => 'paragraph',
-                    'items' => array('NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl')
-                ),
-                array(
-                    'name' => 'links',
-                    'items' => array('Link','Unlink','Anchor')
-                ),
-                array(
-                    'name' => 'insert',
-                    'items' => array('Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak')
-                ),
-                '/',
-                array(
-                    'name' => 'styles',
-                    'items' => array('Styles','Format','Font','FontSize')
-                ),
-                array(
-                    'name' => 'colors',
-                    'items' => array('TextColor','BGColor')
-                ),
-                array(
-                    'name' => 'tools',
-                    'items' => array('Maximize', 'ShowBlocks','-','About')
-                )
-            ),
-            'ui_color' => null
+            'config'  => array(),
         );
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -109,7 +57,7 @@ class CKEditorType extends AbstractType
     {
         return array('required' => array(false));
     }
-    
+
     /**
      * {@inheritdoc}
      */
