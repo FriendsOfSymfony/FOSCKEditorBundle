d8cef6f7bb1ca229361bcf139d240c2d1b020aa2
Eric GELOEN <geloen.eric@gmail.com>
2014-05-27 21:41:59 +0200

    [DependencyInjection] Split configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,87 +26,153 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $node = $treeBuilder->root('ivory_ck_editor');
-
-        $node
+        $treeBuilder = $this->createTreeBuilder();
+        $treeBuilder
+            ->root('ivory_ck_editor')
             ->children()
                 ->booleanNode('enable')->defaultTrue()->end()
                 ->scalarNode('base_path')->defaultValue('bundles/ivoryckeditor/')->end()
                 ->scalarNode('js_path')->defaultValue('bundles/ivoryckeditor/ckeditor.js')->end()
                 ->scalarNode('default_config')->end()
-                ->arrayNode('configs')
-                    ->useAttributeAsKey('name')
-                    ->prototype('array')
-                        ->useAttributeAsKey('name')
-                        ->prototype('variable')->end()
-                    ->end()
+                ->append($this->createConfigsNode())
+                ->append($this->createPluginsNode())
+                ->append($this->createStylesNode())
+                ->append($this->createTemplatesNode())
+                ->append($this->createToolbarsNode())
+            ->end();
+
+        return $treeBuilder;
+    }
+
+    /**
+     * Creates the configs node.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition The configs node.
+     */
+    protected function createConfigsNode()
+    {
+        return $this->createNode('configs')
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+                ->useAttributeAsKey('name')
+                ->prototype('variable')->end()
+            ->end();
+    }
+
+    /**
+     * Creates the plugins node.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition The plugins node.
+     */
+    protected function createPluginsNode()
+    {
+        return $this->createNode('plugins')
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+                ->children()
+                    ->scalarNode('path')->end()
+                    ->scalarNode('filename')->end()
                 ->end()
-                ->arrayNode('plugins')
-                    ->useAttributeAsKey('name')
-                    ->prototype('array')
-                        ->children()
-                            ->scalarNode('path')->end()
-                            ->scalarNode('filename')->end()
+            ->end();
+    }
+
+    /**
+     * Creates the styles node.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition The styles node.
+     */
+    protected function createStylesNode()
+    {
+        return $this->createNode('styles')
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+                ->prototype('array')
+                    ->children()
+                        ->scalarNode('name')->end()
+                        ->scalarNode('element')->end()
+                        ->arrayNode('styles')
+                            ->useAttributeAsKey('name')
+                            ->prototype('scalar')->end()
+                        ->end()
+                        ->arrayNode('attributes')
+                            ->useAttributeAsKey('name')
+                            ->prototype('scalar')->end()
                         ->end()
                     ->end()
                 ->end()
-                ->arrayNode('styles')
-                    ->useAttributeAsKey('name')
-                    ->prototype('array')
+            ->end();
+    }
+
+    /**
+     * Creates the templates node.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition The templates node.
+     */
+    protected function createTemplatesNode()
+    {
+        return $this->createNode('templates')
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+                ->children()
+                    ->scalarNode('imagesPath')->end()
+                    ->arrayNode('templates')
                         ->prototype('array')
                             ->children()
-                                ->scalarNode('name')->end()
-                                ->scalarNode('element')->end()
-                                ->arrayNode('styles')
-                                    ->useAttributeAsKey('name')
-                                    ->prototype('scalar')->end()
-                                ->end()
-                                ->arrayNode('attributes')
-                                    ->useAttributeAsKey('name')
-                                    ->prototype('scalar')->end()
-                                ->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end()
-                ->arrayNode('templates')
-                    ->useAttributeAsKey('name')
-                    ->prototype('array')
-                        ->children()
-                            ->scalarNode('imagesPath')->end()
-                            ->arrayNode('templates')
-                                ->prototype('array')
-                                    ->children()
-                                        ->scalarNode('title')->end()
-                                        ->scalarNode('image')->end()
-                                        ->scalarNode('description')->end()
-                                        ->scalarNode('html')->end()
-                                    ->end()
-                                ->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end()
-                ->arrayNode('toolbars')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->arrayNode('configs')
-                            ->useAttributeAsKey('name')
-                            ->prototype('array')
-                                ->prototype('variable')->end()
-                            ->end()
-                        ->end()
-                        ->arrayNode('items')
-                            ->useAttributeAsKey('name')
-                            ->prototype('array')
-                                ->prototype('variable')->end()
+                                ->scalarNode('title')->end()
+                                ->scalarNode('image')->end()
+                                ->scalarNode('description')->end()
+                                ->scalarNode('html')->end()
                             ->end()
                         ->end()
                     ->end()
                 ->end()
             ->end();
+    }
 
-        return $treeBuilder;
+    /**
+     * Creates the toolbars node.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition The toolbars node.
+     */
+    protected function createToolbarsNode()
+    {
+        return $this->createNode('toolbars')
+            ->addDefaultsIfNotSet()
+            ->children()
+                ->arrayNode('configs')
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                        ->prototype('variable')->end()
+                    ->end()
+                ->end()
+                ->arrayNode('items')
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                        ->prototype('variable')->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * Creates a node.
+     *
+     * @param string $name The node name.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\NodeDefinition The node.
+     */
+    protected function createNode($name)
+    {
+        return $this->createTreeBuilder()->root($name);
+    }
+
+    /**
+     * Creates a tree builder.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder.
+     */
+    protected function createTreeBuilder()
+    {
+        return new TreeBuilder();
     }
 }
