4bffdb915b5a1db175663fa6882d8991682e8c27
Eric GELOEN <geloen.eric@gmail.com>
2017-06-24 21:21:33 +0200

    [Travis] Update docker UID

--- travis/install.sh
+++ travis/install.sh
@@ -8,6 +8,7 @@ DOCKER_BUILD=${DOCKER_BUILD-false}
 
 if [ "$DOCKER_BUILD" = true ]; then
     cp .env.dist .env
+    sed -i -e 's/USER_ID=1000/USER_ID='"$UID"'/g' .env
 
     docker-compose build
     docker-compose run --rm php composer update --prefer-source
