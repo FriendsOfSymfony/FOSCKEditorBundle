8abb879e1f8b0f396f5a35f19b2e543476955d05
Marko Kunic <kunicmarko20@gmail.com>
2018-05-01 09:01:00 +0200

    update licence headers

--- tests/DependencyInjection/FOSCKEditorExtensionTest.php
+++ tests/DependencyInjection/FOSCKEditorExtensionTest.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the FOSCKEditor Bundle.
+ *
+ * (c) 2018 - present  Friends of Symfony
+ * (c) 2009 - 2017     Eric GELOEN <geloen.eric@gmail.com>
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FOS\CKEditorBundle\Tests\DependencyInjection;
 
 use FOS\CKEditorBundle\DependencyInjection\FOSCKEditorExtension;
