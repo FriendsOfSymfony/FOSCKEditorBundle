0b4c3f677d4a8f82860c2f1d72524fa8ad07b15c
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-05-01 07:04:58 +0200

    fix helper call

--- src/Resources/views/Form/_ckeditor_javascript.html.php
+++ src/Resources/views/Form/_ckeditor_javascript.html.php
@@ -1,10 +1,10 @@
 <?php if ($autoload) : ?>
     <script type="text/javascript">
-        var CKEDITOR_BASEPATH = "<?php echo $view['ivory_ckeditor']->renderBasePath($base_path); ?>";
+        var CKEDITOR_BASEPATH = "<?php echo $view['fos_ckeditor']->renderBasePath($base_path); ?>";
     </script>
-    <script type="text/javascript" src="<?php echo $view['ivory_ckeditor']->renderJsPath($js_path); ?>"></script>
+    <script type="text/javascript" src="<?php echo $view['fos_ckeditor']->renderJsPath($js_path); ?>"></script>
     <?php if ($jquery) : ?>
-        <script type="text/javascript" src="<?php echo $view['ivory_ckeditor']->renderJsPath($jquery_path); ?>"></script>
+        <script type="text/javascript" src="<?php echo $view['fos_ckeditor']->renderJsPath($jquery_path); ?>"></script>
     <?php endif; ?>
 <?php endif; ?>
 <script type="text/javascript">
@@ -16,23 +16,23 @@
         require(['ckeditor'], function() {
     <?php endif; ?>
 
-    <?php echo $view['ivory_ckeditor']->renderDestroy($id); ?>
+    <?php echo $view['fos_ckeditor']->renderDestroy($id); ?>
 
     <?php foreach ($plugins as $pluginName => $plugin): ?>
-        <?php echo $view['ivory_ckeditor']->renderPlugin($pluginName, $plugin); ?>
+        <?php echo $view['fos_ckeditor']->renderPlugin($pluginName, $plugin); ?>
     <?php endforeach; ?>
 
     <?php foreach ($styles as $styleName => $style): ?>
-        <?php echo $view['ivory_ckeditor']->renderStylesSet($styleName, $style); ?>
+        <?php echo $view['fos_ckeditor']->renderStylesSet($styleName, $style); ?>
     <?php endforeach; ?>
 
     <?php foreach ($templates as $templateName => $template): ?>
-        <?php echo $view['ivory_ckeditor']->renderTemplate($templateName, $template); ?>
+        <?php echo $view['fos_ckeditor']->renderTemplate($templateName, $template); ?>
     <?php endforeach; ?>
 
     <?php $view['slots']->output('ckeditor_widget_extra', ''); ?>
 
-    <?php echo $view['ivory_ckeditor']->renderWidget(
+    <?php echo $view['fos_ckeditor']->renderWidget(
         $id,
         $config,
         [
