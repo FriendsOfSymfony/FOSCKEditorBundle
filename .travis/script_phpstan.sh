e49f384c194d19f827f259a6ba06d361d0f0f07d
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-05-09 08:06:50 +0200

    make expression easier

--- .travis/script_phpstan.sh
+++ .travis/script_phpstan.sh
@@ -3,3 +3,5 @@
 LEVEL=${1-0}
 
 php vendor/bin/phpstan analyse -l ${LEVEL} src tests
+ARGUMENTS=${LEVEL-null}
+if [ -x .travis/script_${TARGET}.sh ]; then .travis/script_${TARGET}.sh ${ARGUMENTS}; fi
