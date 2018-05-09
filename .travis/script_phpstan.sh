830ecc158b15b4059e700a7cd7a3abdd26b486b4
Marko Kunic <kunicmarko20@gmail.com>
2018-05-09 16:16:06 +0200

    fix travis scripts

--- .travis/script_phpstan.sh
+++ .travis/script_phpstan.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-LEVEL=${1-0}
-
 php vendor/bin/phpstan analyse -l ${LEVEL} src tests
-ARGUMENTS=${LEVEL-null}
-if [ -x .travis/script_${TARGET}.sh ]; then .travis/script_${TARGET}.sh ${ARGUMENTS}; fi
