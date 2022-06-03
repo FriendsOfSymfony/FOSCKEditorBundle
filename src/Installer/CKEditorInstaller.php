32234ea210fac57fa3564a341f8cc670d6a69ff9
Jean-Marc Leroy <info@studio16.be>
2022-06-03 16:48:35 +0200

    Fixed: proxy autoconfiguration for download

--- src/Installer/CKEditorInstaller.php
+++ src/Installer/CKEditorInstaller.php
@@ -205,9 +205,11 @@ class CKEditorInstaller
         $proxy = getenv('https_proxy') ?: getenv('http_proxy');
 
         if ($proxy) {
-            $context['proxy'] = $proxy;
-            $context['request_fulluri'] = (bool) getenv('https_proxy_request_fulluri') ?:
-                getenv('http_proxy_request_fulluri');
+            $context['http'] = [
+                'proxy' => str_replace('/^http\:/, 'tcp:', $proxy),
+                'request_fulluri' => (bool) getenv('https_proxy_request_fulluri') ?:
+                    getenv('http_proxy_request_fulluri'),
+            ];
         }
 
         return stream_context_create($context, [
