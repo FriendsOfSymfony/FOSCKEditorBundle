5bb4e09655d16bc64ece221a3f029bf5b929f063
Christopher Georg <christopher.georg@sr-travel.de>
2025-11-07 16:32:59 +0100

    feat: migrate config xml => php

--- src/Resources/config/renderer.php
+++ src/Resources/config/renderer.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use FOS\CKEditorBundle\Renderer\CKEditorRenderer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
