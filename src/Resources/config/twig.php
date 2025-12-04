6aad9cd12423f0522d386cb461c997135a7eed36
Charles <1032281+XWB@users.noreply.github.com>
2025-12-04 19:57:05 +0100

    Update twig.php
    Import service function

--- src/Resources/config/twig.php
+++ src/Resources/config/twig.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use FOS\CKEditorBundle\Twig\CKEditorExtension;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
